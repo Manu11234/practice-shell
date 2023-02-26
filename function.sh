a38c5b33a53d7cd87346787660217584f40816dc
Manohar <manu.batchu@gmail.com>
2023-02-26 17:52:02 +0530

    function practice

--- function.sh
+++ function.sh
@@ -7,8 +7,11 @@ xyz (){
   echo third argument - $3
   echo all arguments - $*
   echo number of arguments - $#
+  echo value of a = $a
+  b=55
 }
-
+##main program
 ##call function
 xyz 123 456 789
-
+a=25
+echo "value of b = $b"
