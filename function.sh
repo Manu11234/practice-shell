89b951b1599cd087fa3b70aecc75031bfc384485
Manohar <manu.batchu@gmail.com>
2023-02-26 17:46:43 +0530

    function practice

--- function.sh
+++ function.sh
@@ -2,8 +2,13 @@
 dir=$(pwd)
 xyz (){
   echo hello from function
+  echo first argument - $1
+  echo second argument - $2
+  echo third argument - $3
+  echo all arguments - $*
+  echo number of arguments - $#
 }
 
 ##call function
-xyz
+xyz 123 456 789
 
