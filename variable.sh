1ffc90aa0dc303d31bb812154c8503b5d6bcf5aa
Manohar <manu.batchu@gmail.com>
2023-02-26 12:16:52 +0530

    variable practice

--- variable.sh
+++ variable.sh
@@ -1,7 +1,10 @@
 DATE=26-02-2023
 echo "Hi, everyone, Good Morning ${DATE}"
+# command substitution
 DATE=$(date)
 echo "Hi Hello world ${DATE}"
 
-
+#Arithmatic substitution
+Add=$((5+3))
+echo "addition of 3+5= ${ADD}"
 
