218da5b4422bdf9a083b83cd43d1ccf2bbf6d7e5
Manohar <manu.batchu@gmail.com>
2023-02-26 12:43:40 +0530

    inputs practice

--- inputs.sh
+++ inputs.sh
@@ -1,2 +1,11 @@
-echo $1
-echo $2
+# 0-n , $*/$@ , $#
+
+echo First Argument $1 #First Argument
+echo Second Argument $2 #Second Argument
+
+echo Script name $0 #Script name
+
+echo All arguments = $* #All arguments
+echo All arguments = $@ #All arguments
+
+echo $# #Number of arguments
